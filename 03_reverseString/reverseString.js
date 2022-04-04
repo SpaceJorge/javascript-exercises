3dd6ef0c8c1b8d79bf7be2956c5c72c69f277cbc
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-04 16:18:36 -0300

    Exercise 03 finished

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversed = "";
+    if (string && (string.length > 0) ) {
+        for (let i = 0; i<string.length; i++){
+            reversed = string.charAt(i) + reversed;
+        }
+        return reversed
+    }
+    return ""
 };
 
 // Do not edit below this line
