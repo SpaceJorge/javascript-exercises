fb8b99c55f260cbc31ea68f040eeed9b2b90a63a
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-04 15:54:31 -0300

    excercise 01 and 02 finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    let repeatedText = "";
+    if (num < 0){
+        return "ERROR"
+    }else if (num == 0){
+        return ""
+    }else{
+        for (let i = 0;i<num;i++){
+            repeatedText = repeatedText + string;
+        }
+        return repeatedText
+    }
 };
 
 // Do not edit below this line
