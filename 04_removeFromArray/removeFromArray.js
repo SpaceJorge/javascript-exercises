6ec9091103f980f0be21ee8dd76e8e911b359fe5
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-04 18:39:28 -0300

    Excercise 04 finished

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...unwantedItems) {
+    const newArray = [];
+    let flag = 0;
+    for (const item of array){
+       
+        for (const unwanted of unwantedItems){
+        
+            if (unwanted === item) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0) {
+                newArray.push(item);
+        }else{
+            flag = 0;
+        }
+    }
+
+    return newArray
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
