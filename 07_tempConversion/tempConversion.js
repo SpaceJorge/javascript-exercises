20bb609db28c521f42072481689eb7798a3b05b9
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-05 13:57:48 -0300

    Exercise 07 finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = 0;
+  celcius = (farenheit -32)*(5/9);
+  celcius = Math.round(celcius*10)/10;
+  return celcius
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let farenheit = 0;
+  farenheit = celcius * (9/5) + 32;
+  farenheit = Math.round(farenheit*10)/10;
+  return farenheit
 };
 
 // Do not edit below this line
