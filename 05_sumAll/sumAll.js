dfc70f6977e791a69a6ec2b6db89efd36a3aea1f
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-05 13:39:30 -0300

    Exercise 05 finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numOne,numTwo) {
+    if ( (typeof(numOne) != "number") || (typeof(numTwo) != "number") ) {
+        return "ERROR"
+    } else if ( (numOne < 0) || (numTwo < 0) ) {
+        return "ERROR"
+    }
+    
+    let sum = 0;
+
+    if ( numOne == numTwo ) {
+        return numOne
+    } else if (numOne>numTwo) {
+        for (let i = numTwo; i<=numOne ;i++){
+            sum = sum + i;
+        }
+        return sum
+    } else{
+        for (let i = numOne; i<=numTwo ;i++){
+            sum = sum + i;
+        }
+        return sum
+    }
 
 };
 
