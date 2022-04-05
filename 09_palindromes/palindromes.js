8d71ee599cca49d3b64fdb101e3e61e5e59d4cb8
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-05 15:41:33 -0300

    Exercise 09 finished

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const oldString = string.toLowerCase();
+    let onlyLetterString = "";
+    for (let i = 0; i<= (string.length-1); i++){
+        if ((oldString.charAt(i)>="a")&& (oldString.charAt(i)<="z") ){
+            onlyLetterString = onlyLetterString + oldString.charAt(i);
+        }
+    }
+    
+    for (let j = 0; j <= (onlyLetterString.length-1); j++ ){
+        if ( onlyLetterString.charAt(j) == (onlyLetterString.charAt(onlyLetterString.length-1-j)) ){
+            continue;
+        } else {
+            return false
+        }
+    }
+    return true
 
 };
 
