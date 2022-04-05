93c8c2c4f58e812426e39e0c7ef37c85ed64a392
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-05 16:04:36 -0300

    Exercise 10 finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let series = [1,1];
+    let sum = 1;
+    if ((num == 1) || (num == 2)){
+        return 1
+    } else if (num < 1){
+        return "OOPS"
+    }
+    for (let i = 1; i<=(num-2); i++){
+        sum = series[i] + series[i-1];
+        series.push(sum);
+    }
+    return series.pop()
 };
 
 // Do not edit below this line
