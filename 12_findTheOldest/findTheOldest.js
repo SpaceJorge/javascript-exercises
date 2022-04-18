e6d09672452cd5f378dc73cfb3c350a786ce834e
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-04-18 19:11:54 -0300

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let isSomeoneAlive = false;
+    
+    const chosen = people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        if (!currentPerson.dateOfDeath){   
+            isSomeoneAlive = true;
+        }
 
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
+   
+    /*if (isSomeoneAlive == true){
+        return chosen;
+    }
+    if (!chosen.yearOfDeath){
+        return chosen;
+    }*/
+    
 };
+    
+const getAge = function(birth, death) {
+    if (!death) {
+    death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
